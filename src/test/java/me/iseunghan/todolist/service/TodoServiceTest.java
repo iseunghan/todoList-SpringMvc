645408746f3e9490c785263b9ecd1e578c32e60c
iseunghan <tmdgks301@naver.com>
2021-05-20 18:01:00 +0900

    docs: update README.md

--- src/test/java/me/iseunghan/todolist/service/TodoServiceTest.java
+++ src/test/java/me/iseunghan/todolist/service/TodoServiceTest.java
@@ -2,7 +2,9 @@ package me.iseunghan.todolist.service;
 
 import me.iseunghan.todolist.model.TodoItem;
 import me.iseunghan.todolist.model.TodoStatus;
+import me.iseunghan.todolist.model.User;
 import me.iseunghan.todolist.repository.TodoRepository;
+import me.iseunghan.todolist.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class TodoServiceTest {
@@ -24,18 +24,25 @@ class TodoServiceTest {
     TodoService todoService;
     @Autowired
     TodoRepository todoRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     @DisplayName("할일 추가 기능 테스트")
     void addTodoTest() {
         // given
+        User user = new User();
+        user.setName("name");
+        user.setEmail("email@naver.com");
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
         TodoItem todoItem = new TodoItem();
         todoItem.setTitle("스프링 공부하기");
         todoItem.setDate(LocalDate.parse("2020-01-25"));
         todoItem.setStatus(TodoStatus.NEVER);
 
         // when
-        TodoItem todoItem2 = todoService.addTodo(todoItem);
+        TodoItem todoItem2 = todoService.addTodo(todoItem, user.getId());
 
         // then
         assertEquals(todoItem2.getTitle(), todoItem.getTitle());
@@ -47,6 +54,11 @@ class TodoServiceTest {
     @DisplayName("할일 목록 조회 테스트")
     void getTodoItemListTest() {
         // given
+        User user = new User();
+        user.setName("name");
+        user.setEmail("email@naver.com");
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
         TodoItem todoItem1 = new TodoItem();
         todoItem1.setTitle("자바 공부하기");
         todoItem1.setDate(LocalDate.parse("2020-01-25"));
@@ -57,8 +69,8 @@ class TodoServiceTest {
         todoItem2.setDate(LocalDate.parse("1990-12-13"));
         todoItem2.setStatus(TodoStatus.NEVER);
 
-        todoService.addTodo(todoItem1);
-        todoService.addTodo(todoItem2);
+        todoService.addTodo(todoItem1, user.getId());
+        todoService.addTodo(todoItem2, user.getId());
 
         // when
         List<TodoItem> list = todoService.getTodoItemList();
@@ -77,6 +89,12 @@ class TodoServiceTest {
     @DisplayName("날짜로 조회 테스트")
     void findTodoListByDateTest() {
         // given
+        User user = new User();
+        user.setName("name");
+        user.setEmail("email@naver.com");
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+
         TodoItem todoItem1 = new TodoItem();
         todoItem1.setTitle("자바 공부하기");
         LocalDate date = LocalDate.of(2020, 01, 25);
@@ -94,9 +112,9 @@ class TodoServiceTest {
         todoItem3.setDate(date1);
         todoItem3.setStatus(TodoStatus.NEVER);
 
-        todoService.addTodo(todoItem1);
-        todoService.addTodo(todoItem2);
-        todoService.addTodo(todoItem3);
+        todoService.addTodo(todoItem1, user.getId());
+        todoService.addTodo(todoItem2, user.getId());
+        todoService.addTodo(todoItem3, user.getId());
 
         // when
         List<TodoItem> list = todoService.findTodoListByDate(date);
@@ -112,11 +130,17 @@ class TodoServiceTest {
     @Test
     void updateStatusTest() {
         // Given
+        User user = new User();
+        user.setName("name");
+        user.setEmail("email@naver.com");
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+
         TodoItem todoItem = new TodoItem();
         todoItem.setTitle("책 읽기");
         todoItem.setStatus(TodoStatus.NEVER);
         todoItem.setDate(LocalDate.now());
-        todoService.addTodo(todoItem);
+        todoService.addTodo(todoItem, user.getId());
 
         // When
         todoService.updateStatus(todoItem.getId());
