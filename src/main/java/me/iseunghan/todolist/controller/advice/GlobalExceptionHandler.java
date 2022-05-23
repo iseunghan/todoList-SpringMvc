800371a7517fb362678121354f2931ceb8df37b6
iseunghan <tmdgks301@naver.com>
2022-05-23 21:10:54 +0900

    #9 ExceptionHandler 추가

--- src/main/java/me/iseunghan/todolist/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/me/iseunghan/todolist/controller/advice/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package me.iseunghan.todolist.controller.advice;
 
+import me.iseunghan.todolist.exception.AccountDuplicateException;
 import me.iseunghan.todolist.exception.NotEmptyException;
-import me.iseunghan.todolist.exception.NotFoundException;
+import me.iseunghan.todolist.exception.TodoNotFoundException;
+import me.iseunghan.todolist.exception.AccountNotFoundException;
 import me.iseunghan.todolist.exception.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,13 +17,25 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotEmptyException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse _400(NotEmptyException e) {
+    public ErrorResponse _todo_400(NotEmptyException e) {
         return new ErrorResponse(e.getId(), e.getMessage());
     }
 
-    @ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(TodoNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse _404(NotFoundException e) {
+    public ErrorResponse _todo_404(TodoNotFoundException e) {
         return new ErrorResponse(e.getId(), e.getMessage());
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse _user_404(AccountNotFoundException e) {
+        return new ErrorResponse(e.getUsername(), e.getMessage());
+    }
+
+    @ExceptionHandler(AccountDuplicateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse _user_400(AccountDuplicateException e) {
+        return new ErrorResponse(e.getUsername(), e.getMessage());
     }
 }
